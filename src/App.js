5a63c4d9b25039e720d4c86079c10feb0ad57c81
Liam Vermette <Liam.vermette@gmail.com>
2024-06-05 15:45:14 -0400

    added navbar and demo of css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar
+        bg="light"
+        expand="lg"
+        style={{ display: "flex", 
+        "flex-direction": "row", 
+        margin: "2px", 
+        "background-color": "whitesmoke", //example of quotes css to contain dash
+        padding: '30px', //doesn't need quotes
+        justifyContent: "space-around" //example of camel case react version (both work!)
+      }}
+      >
+        <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
+          <img src="[Instacart logo src]" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{" "}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
